8b72ddd4e4ef56d7c853e73eb937cf42ba234d25
Pablo Cordero <pablocorderoromero@gmail.com>
2022-10-09 18:39:43 +0200

    feat: add shortcut to tip

--- src/config.py
+++ src/config.py
@@ -2,5 +2,5 @@ import os
 
 CMD = "github_markdown"
 SHORTCUT = "Ctrl+Alt+M"
-DESCRIPTION = "Convert to markdown Github"
+DESCRIPTION = f"Convert to markdown ({SHORTCUT})"
 BUTTON_ICON_PATH = os.path.join(os.path.dirname(__file__), "assets/logo.png")
